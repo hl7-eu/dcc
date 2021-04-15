722d88fc2e8a0eeeae87bcb8e7dc3fd6a89b0895
danka74 <daniel.karlsson@socialstyrelsen.se>
2021-04-15 12:11:52 +0200

    script to generate VS from JRC test JSON file

---
+++ gen_test_vs.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# a script to generate a FHIR ValueSet from the JSON file provided by JRC
+jq '{ 
+  "resourceType": "ValueSet", 
+  "status": "draft", 
+  "name": "Covid19LabTestManufacturerAndName", 
+  "id": "covid-19-lab-test-manufacturer-and-name", 
+  "title": "COVID-19 Lab Test Manufacturer And Name", 
+  "description": "Examples of COVID-19 Lab Test Manufacturer And Name from http://covid-19-diagnostics.jrc.ec.europa.eu/devices?manufacturer&text_name&marking&rapid_diag&format&target_type&field-1=HSC%20mutual%20recognition%20%28RAT%29&value-1=1&search_method=AND#form_content",
+  "version": "0.0.1", 
+  "url": "http://hl7.eu/fhir/ig/dgc/ValueSet/covid-19-lab-test-manufacturer-and-name", 
+  "compose": { 
+    "include": [ 
+      { 
+        "system": "https://covid-19-diagnostics.jrc.ec.europa.eu/devices", 
+        "concept": [ .deviceList | .[] | { "code": .id_device, "display": (.manufacturer.name + ", " + .commercial_name) }
+] } ] } }' $1
+
